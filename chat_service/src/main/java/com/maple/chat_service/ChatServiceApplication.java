23b43c9f54cdf628efa690a6112b895103f31ee0
wangchang0408 <505162243@qq.com>
2020-09-17 15:58:56 +0800

    聊天后台

---
+++ chat_service/src/main/java/com/maple/chat_service/ChatServiceApplication.java
@@ -0,0 +1,16 @@
+package com.maple.chat_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatServiceApplication.class, args);
+	}
+
+}
